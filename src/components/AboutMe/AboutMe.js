0c6bad1d3845615eaf9d288c311bca2e9a90efed
Ekaterina Nechkina <e.nechkina@yandex.ru>
2023-06-21 22:53:28 +0300

    add target blank

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -11,7 +11,7 @@ function AboutMe() {
           <p className="about__profile">Фронтенд-разработчик, 30 лет</p>
           <p className="about__description">Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
             и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
-          <a className="about__url" href="https://github.com/meow-gustera/">Github</a>
+          <a className="about__url" href="https://github.com/meow-gustera/" target="_blank" rel="noreferrer">Github</a>
         </div>
         <img className="about__photo" src={photo} alt="Фото профиля" />
       </div>
