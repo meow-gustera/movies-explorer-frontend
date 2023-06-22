88f024f22c9c1727a18640b11e751bfae9dd719b
Ekaterina Nechkina <e.nechkina@yandex.ru>
2023-06-22 15:58:49 +0300

    add urls to portfolio__item

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,16 +6,22 @@ function Portfolio() {
       <h3 className="portfolio__title">Портфолио</h3>
       <ul className="portfolio__items">
         <li className="portfolio__item">
-          <a className="portfolio__item-link" href="https://github.com/meow-gustera/how-to-learn" target="_blank" rel="noreferrer">Статичный сайт</a>
-          <p className="portfolio__item-symbol">↗</p>
+          <a className="portfolio__item-link" href="https://github.com/meow-gustera/how-to-learn" target="_blank" rel="noreferrer">
+            <p className="portfolio__item-name">Статичный сайт</p>
+            <p className="portfolio__item-symbol">↗</p>
+          </a>
         </li>
         <li className="portfolio__item">
-          <a className="portfolio__item-link" href="https://github.com/meow-gustera/russian-travel" target="_blank" rel="noreferrer">Адаптивный сайт</a>
-          <p className="portfolio__item-symbol">↗</p>
+          <a className="portfolio__item-link" href="https://github.com/meow-gustera/russian-travel" target="_blank" rel="noreferrer">
+            <p className="portfolio__item-name">Адаптивный сайт</p>
+            <p className="portfolio__item-symbol">↗</p>
+          </a>
         </li>
         <li className="portfolio__item">
-          <a className="portfolio__item-link" href="https://github.com/meow-gustera/react-mesto-api-full-gha" target="_blank" rel="noreferrer">Одностраничное приложение</a>
-          <p className="portfolio__item-symbol">↗</p>
+          <a className="portfolio__item-link" href="https://github.com/meow-gustera/react-mesto-api-full-gha" target="_blank" rel="noreferrer">
+            <p className="portfolio__item-name">Одностраничное приложение</p>
+            <p className="portfolio__item-symbol">↗</p>
+          </a>
         </li>
       </ul>
     </section>
